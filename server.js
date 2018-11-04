c853e1dd7c8a2e65682145a17f5140f8fa2cb9b9
ankit14ap <ankitp@trulymadly.com>
2018-11-04 18:58:11 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,43 @@
+// console.log('Server is starting...1');
+
+const express=require('express');
+var app=express();
+const hbs=require('hbs');
+app.set('view engine',hbs);
+app.use(express.static(__dirname+'/public'));
+const data={
+		project: 'boloo',
+		team:[
+		'Amit',
+		'Ankit',
+		'Nandu',
+		'Apurv'
+		]
+	};
+
+var arr=new Array();
+for (var i = 0; i <= 10; i++) {
+	arr[i]=data;
+}
+app.get('/',(req,res)=>{
+	res.render('home.hbs',{
+		title: 'home',
+		currentYear: new Date().getFullYear()
+	});
+	// res.send(arr);
+})
+
+app.get('/bad',(req,res)=>{
+	res.send('bad');
+})
+app.get('/about',(req,res)=>{
+	res.render('about.hbs',{
+		title: 'about dynamic',
+		currentYear: new Date().getFullYear()
+	});
+})
+
+
+app.listen(3000,()=>{
+	console.log('Server is hoting on port 3000');
+});
